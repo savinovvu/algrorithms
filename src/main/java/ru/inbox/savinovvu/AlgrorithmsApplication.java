2c7b350ad0d75b6467b188dbbed4f7ab469c50b6
Vasiliy Savinov <savinov_vu@inbox.ru>
2023-02-07 12:12:52 +0800

    start

--- src/main/java/ru/inbox/savinovvu/AlgrorithmsApplication.java
+++ src/main/java/ru/inbox/savinovvu/AlgrorithmsApplication.java
@@ -6,14 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AlgrorithmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlgrorithmsApplication.class, args);
-
-
-
-
-
-		                               System.out.println("asdfasdfasdfasdf");
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AlgrorithmsApplication.class, args);
+  }
 
 }
