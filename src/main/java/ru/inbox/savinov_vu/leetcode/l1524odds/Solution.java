e5b99565215c62a3e1e2831bf7e4b4cd046acace
Vasiliy Savinov <savinov_vu@inbox.ru>
2023-02-13 16:59:38 +0800

    1524.odds

---
+++ src/main/java/ru/inbox/savinov_vu/leetcode/l1524odds/Solution.java
@@ -0,0 +1,16 @@
+package ru.inbox.savinov_vu.leetcode.l1524odds;
+
+class Solution {
+
+  public int countOdds(int low, int high) {
+
+    int result = low % 2 == 1 || high % 2 == 1 ? 1 : 0;
+    if (low == high) {
+      return result;
+    }
+
+    result += (high - low) / 2;
+
+    return result;
+  }
+}
---
+++ src/test/java/ru/inbox/savinov_vu/leetcode/l1524odds/SolutionTest.java
@@ -0,0 +1,46 @@
+package ru.inbox.savinov_vu.leetcode.l1524odds;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+  @Test
+  public void test1() {
+    int i = new Solution().countOdds(9, 10);
+    assertThat(i).isEqualTo(1);
+  }
+
+  @Test
+  public void test2() {
+    int i = new Solution().countOdds(3, 7);
+    assertThat(i).isEqualTo(3);
+  }
+
+  @Test
+  public void test3() {
+    int i = new Solution().countOdds(0, 0);
+    assertThat(i).isEqualTo(0);
+  }
+
+  @Test
+  public void test4() {
+    int i = new Solution().countOdds(0, 1);
+    assertThat(i).isEqualTo(1);
+  }
+
+  @Test
+  public void test5() {
+    int i = new Solution().countOdds(3, 3);
+    assertThat(i).isEqualTo(1);
+  }
+
+  @Test
+  public void test6() {
+    int i = new Solution().countOdds(8, 10);
+    assertThat(i).isEqualTo(1);
+  }
+
+
+}
