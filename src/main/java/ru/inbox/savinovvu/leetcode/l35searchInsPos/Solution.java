4f2614356c1a9b0b9f48f4de51ee227e45509165
Vasiliy Savinov <savinov_vu@inbox.ru>
2023-02-20 15:55:33 +0800

    l35 search int position

--- src/main/java/ru/inbox/savinovvu/leetcode/l35searchInsPos/Solution.java
+++ src/main/java/ru/inbox/savinovvu/leetcode/l35searchInsPos/Solution.java
@@ -1,50 +1,18 @@
 package ru.inbox.savinovvu.leetcode.l35searchInsPos;
 
 class Solution {
-
   public int searchInsert(int[] nums, int target) {
-    int min = 0;
-    int max = nums.length - 1;
-    Integer result = findOrder(nums, target, min, max);
+    int start = 0;
+    int end = nums.length - 1;
 
-    return result < 0 ? 0 : result;
-  }
-
-  private Integer findOrder(int[] nums, int target, int min, int max) {
-    if (target == nums[min]) {
-      return min;
-    }
-    if (target == nums[max]) {
-      return max;
+    while (start <= end) {
+      int mid = start + (end-start)/2;
+      if (nums[mid] == target) return mid;
+      else if (nums[mid] > target) end = mid-1;
+      else start = mid + 1;
     }
 
-    if (target < nums[min]) {
-      return min - 1;
-    }
-
-    if (target > nums[max]) {
-      return max + 1;
-    }
-    if (min + 1 == max) {
-      return min + 1;
-    }
-
-
-    int middle = getMiddle(min, max);
-
-    if (target < nums[middle]) {
-      return findOrder(nums, target, min, middle);
-    }
-
-    if (target > nums[middle]) {
-      return findOrder(nums, target, middle, max);
-    }
-
-    return middle;
-  }
-
-  public int getMiddle(int min, int max) {
-    return (max + min) / 2;
+    return start;
   }
 
 }
