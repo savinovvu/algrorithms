4ceb73408f774b8ce513b4b490a3cb94d1885b10
Vasiliy Savinov <savinov_vu@inbox.ru>
2023-02-20 13:41:46 +0800

    989. array plus integer

--- src/main/java/ru/inbox/savinovvu/leetcode/l989arrayplusinteger/Solution.java
+++ src/main/java/ru/inbox/savinovvu/leetcode/l989arrayplusinteger/Solution.java
@@ -1,36 +1,20 @@
 package ru.inbox.savinovvu.leetcode.l989arrayplusinteger;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
 
-  public List<Integer> addToArrayForm(int[] num, int k) {
-
-    int length = num.length;
-    long sum = k;
-    List<Integer> result = new ArrayList<>();
-    for (int i = length - 1; i >= 0; i--) {
-      long degree = (long) Math.pow(10, length - 1 - i);
-      sum = (long) num[i] * degree + sum;
-      long degreeToGetDigit = degree * 10;
-      long digit = (sum % degreeToGetDigit) / degree;
-      result.add(0, (int)digit);
+  public List<Integer> addToArrayForm(int[] num, int K) {
+    List<Integer> res = new LinkedList<>();
+    for (int i = num.length - 1; i >= 0; --i) {
+      res.add(0, (num[i] + K) % 10);
+      K = (num[i] + K) / 10;
     }
-
-    long sumLength = String.valueOf(sum).length();
-    long resultSize = result.size();
-
-    if (sumLength > resultSize) {
-      double addition = (long)sum / Math.pow(10, resultSize);
-
-      while (addition >= 1) {
-        long digit = (long) addition % 10;
-        result.add(0, (int)digit);
-        addition = addition / 10;
-      }
-
+    while (K > 0) {
+      res.add(0, K % 10);
+      K /= 10;
     }
-    return result;
+    return res;
   }
 }
