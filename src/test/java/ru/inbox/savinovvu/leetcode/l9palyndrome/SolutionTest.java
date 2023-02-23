9942ea5e1b9eaa49df6abcc572c3bbbe54ad238e
skorpion <savinov_vu@inbox.ru>
2023-02-23 22:56:02 +0800

    l9 check palindrom

---
+++ src/test/java/ru/inbox/savinovvu/leetcode/l9palyndrome/SolutionTest.java
@@ -0,0 +1,38 @@
+package ru.inbox.savinovvu.leetcode.l9palyndrome;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+
+  @Test
+  public void test1() {
+    boolean palindrome = new Solution().isPalindrome(0);
+    assertThat(palindrome).isEqualTo(true);
+  }
+
+  @Test
+  public void test2() {
+    boolean palindrome = new Solution().isPalindrome(-121);
+    assertThat(palindrome).isEqualTo(false);
+  }
+
+  @Test
+  public void test3() {
+    boolean palindrome = new Solution().isPalindrome(121);
+    assertThat(palindrome).isEqualTo(true);
+  }
+
+  @Test
+  public void test4() {
+    boolean palindrome = new Solution().isPalindrome(1210);
+    assertThat(palindrome).isEqualTo(false);
+  }
+
+  @Test
+  public void test5() {
+    boolean palindrome = new Solution().isPalindrome(1221);
+    assertThat(palindrome).isEqualTo(true);
+  }
+}
