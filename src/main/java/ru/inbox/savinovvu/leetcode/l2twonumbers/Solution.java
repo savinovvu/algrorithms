87e64105eb55d2f02a4073cabf158adc7b3ae6bd
Vasiliy Savinov <savinov_vu@inbox.ru>
2023-02-23 11:51:12 +0800

    l35 search int position

--- src/main/java/ru/inbox/savinovvu/leetcode/l2twonumbers/Solution.java
+++ src/main/java/ru/inbox/savinovvu/leetcode/l2twonumbers/Solution.java
@@ -1,8 +1,36 @@
 package ru.inbox.savinovvu.leetcode.l2twonumbers;
 
+import java.util.LinkedList;
+import java.util.Optional;
+
 class Solution {
 
   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    return null;
+
+    LinkedList<Object> objects = new LinkedList<>();
+    ListNode first = new ListNode();
+
+    addNodes(l1, l2, first);
+    ListNode listNode = new ListNode();
+    new ListNode();
+
+    return first;
   }
+
+  private void addNodes(ListNode l1, ListNode l2, ListNode result) {
+    int sum = l1.val + l2.val;
+    if (sum == 0) {
+      return;
+    }
+    result.val = sum;
+    int decimals = sum / 10;
+    result.next = new ListNode(decimals);
+    ListNode l1Next = Optional.ofNullable(l1.next).orElse(new ListNode());
+    ListNode l2Next = Optional.ofNullable(l2.next).orElse(new ListNode());
+    addNodes(l1, l2, result.next);
+  }
+
+
+
+
 }
