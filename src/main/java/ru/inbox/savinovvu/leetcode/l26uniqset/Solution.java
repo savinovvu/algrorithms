ceaf913d1ad59ac6e6187eb62157cc51b387ae5e
skorpion <savinov_vu@inbox.ru>
2023-02-24 15:08:13 +0800

    l26 merge two list

---
+++ src/main/java/ru/inbox/savinovvu/leetcode/l26uniqset/Solution.java
@@ -0,0 +1,9 @@
+package ru.inbox.savinovvu.leetcode.l26uniqset;
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+class Solution {
+
+  public int removeDuplicates(int[] nums) {
+    return 0;
+  }
+}
---
+++ src/test/java/ru/inbox/savinovvu/leetcode/l26uniqset/SolutionTest.java
@@ -0,0 +1,17 @@
+package ru.inbox.savinovvu.leetcode.l26uniqset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+  @Test
+  public void test1() {
+    assertThat(1).isEqualTo(1);
+  }
+  @Test
+  public void test2() {
+    assertThat(1).isEqualTo(1);
+  }
+}
