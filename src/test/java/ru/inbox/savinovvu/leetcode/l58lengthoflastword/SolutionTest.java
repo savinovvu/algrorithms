6e36c83e3204f552bb639ee9be5f4a03814c947e
Vasiliy Savinov <savinov_vu@inbox.ru>
2023-02-27 13:04:55 +0800

    l27 remove digit from array

---
+++ src/test/java/ru/inbox/savinovvu/leetcode/l58lengthoflastword/SolutionTest.java
@@ -0,0 +1,29 @@
+package ru.inbox.savinovvu.leetcode.l58lengthoflastword;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+  @Test
+  public void test1() {
+    int res = new Solution().lengthOfLastWord("Hello world");
+    assertThat(res).isEqualTo(5);
+  }
+  @Test
+  public void test2() {
+    int res = new Solution().lengthOfLastWord("   fly me   to   the moon  ");
+    assertThat(res).isEqualTo(4);
+  }
+  @Test
+  public void test3() {
+    int res = new Solution().lengthOfLastWord("moon");
+    assertThat(res).isEqualTo(4);
+  }
+  @Test
+  public void test4() {
+    int res = new Solution().lengthOfLastWord(" ");
+    assertThat(res).isEqualTo(0);
+  }
+}
