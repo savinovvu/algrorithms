7d109fedff3ce9793e166aa154d4ebf07794ac4c
Vasiliy Savinov <savinov_vu@inbox.ru>
2023-12-15 09:19:22 +0800

    12 integer to roman

---
+++ java/src/main/kotlin/ru/inbox/savinovvu/kotlin/l12IntegerToRoman/Solution.kt
@@ -0,0 +1,23 @@
+package ru.inbox.savinovvu.kotlin.l12IntegerToRoman
+
+
+fun main() {
+    val values = listOf(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    val symbols = listOf("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
+
+    fun intToRoman(num: Int): String {
+        var result = ""
+        var i = 0
+        var number = num
+        while (i < values.size) {
+            while (values[i] <= number) {
+                number -= values[i]
+                result += symbols[i]
+            }
+            i += 1
+        }
+        return result
+    }
+
+    println(intToRoman(9))
+}
