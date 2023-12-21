8e146253b9e1fc74c54ceb8757fdb440332de8b7
Vasiliy Savinov <savinov_vu@inbox.ru>
2023-12-21 09:11:21 +0800

    797

--- src/main/java/ru/inbox/savinovvu/leetcode/l797/Solution.java
+++ src/main/java/ru/inbox/savinovvu/leetcode/l797/Solution.java
@@ -1,6 +1,7 @@
 package ru.inbox.savinovvu.leetcode.l797;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
@@ -9,27 +10,20 @@ public class Solution {
     int[][] graph1 = {{1, 2}, {3}, {3}, {}};
     int[][] graph2 = {{4, 3, 1}, {3, 2, 4}, {3}, {4}, {}};
 
-    System.out.println("Test 1: " + new Solution().allPathsSourceTarget(graph1));
-    System.out.println("Test 2: " + new Solution().allPathsSourceTarget(graph2));
+    System.out.println("Test 1: " + new Solution().allPathsSourceTarget3(graph1));
+    System.out.println("Test 2: " + new Solution().allPathsSourceTarget3(graph2));
   }
 
-//  Map<Integer, List<List<Integer>>> map = new HashMap<>();
 
   public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-    List<List<Integer>> result = new ArrayList<>();
     List<Integer> current = new ArrayList<>();
     current.add(0);
     List<List<Integer>> dfs = dfs(0, graph, current);
     return dfs;
   }
 
+
   private List<List<Integer>> dfs(int point, int[][] graph, List<Integer> current) {
-//    if (map.containsKey(point)) {
-//      return map.get(point);
-//    }
-
-//    current.add(point);
-
     List<List<Integer>> result = new ArrayList<>();
     for (int node : graph[point]) {
       List<Integer> branch = new ArrayList<>(current);
@@ -42,6 +36,51 @@ public class Solution {
       result.addAll(dfs);
     }
     return result;
+  }
+// ------------
+public List<List<Integer>> allPathsSourceTarget2(int[][] graph) {
+  return dfs2(0, graph, new ArrayList<>(Arrays.asList(0)));
+}
+
+
+  private List<List<Integer>> dfs2(int point, int[][] graph, List<Integer> path) {
+    List<List<Integer>> paths = new ArrayList<>();
+    if (point == graph.length - 1) {
+      paths.add(new ArrayList<>(path));
+      return paths;
+    }
+    for (int node : graph[point]) {
+      path.add(node);
+      paths.addAll(dfs2(node, graph, path));
+      path.remove(path.size() - 1);
+    }
+    return paths;
+  }
+
+// ----------------
+
+
+  public List<List<Integer>> allPathsSourceTarget3(int[][] graph) {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
+    path.add(0);
+    dfs3(graph, 0, result, path);
+
+    return result;
+  }
+
+  private void dfs3(int[][] graph, int node, List<List<Integer>> result, List<Integer> path) {
+    if (node == graph.length - 1) {
+      result.add(new ArrayList<>(path));
+      return;
+    }
+
+    for (int nextNode : graph[node]) {
+      path.add(nextNode);
+      dfs3(graph, nextNode, result, path);
+      path.remove(path.size() - 1);
+    }
   }
 
 }
